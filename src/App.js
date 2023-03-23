21ab353a179f71edd8379b69ea666ec8898c04d9
cys0926 <cys0926ok@gmail.com>
2023-03-24 01:23:26 +0900

    feat : App.js 에 Icons, Footer 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
-import router from "./core/Router";
+import router from './core/Router';
+import { html } from './utils/utils';
+import WebComponent from './core/WebComponent';
 
-export default class App extends HTMLElement {
-  connectedCallback() {
+export default class App extends WebComponent {
+  constructor() {
+    super();
     router.render();
-    window.addEventListener("popstate", () => router.render());
+  }
+
+  connectedCallback() {
+    super.connectedCallback();
+    window.addEventListener('popstate', () => router.render());
+  }
+
+  injectHTML() {
+    return html`
+      <my-icons slot="icons" tabindex="-1"></my-icons>
+      <my-footer slot="footer"></my-footer>
+    `;
   }
 }
