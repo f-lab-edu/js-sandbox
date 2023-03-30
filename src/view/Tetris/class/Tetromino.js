2d60455c00a78ed2ef9df0ae38f1336f105bc141
cys0926 <cys0926ok@gmail.com>
2023-03-31 07:01:07 +0900

    feat : 한줄 채워지는 경우 스코어 증가 구현

--- src/view/Tetris/class/Tetromino.js
+++ src/view/Tetris/class/Tetromino.js
@@ -1,17 +1,38 @@
-import { BLOCKS, BOARD_HEIGHT, BOARD_WIDTH } from '../const';
+import { BLOCKS, BOARD_HEIGHT, BOARD_WIDTH, SCORES } from '../const';
 
 export default class Tetromino {
   constructor() {
-    const block = Object.values(BLOCKS)[Math.floor(Math.random() * 7)];
-    const initialBlockPos = this.getInitialBlockPos(BOARD_WIDTH, block);
-
-    this.block = block;
+    this.block = Object.values(BLOCKS)[Math.floor(Math.random() * 7)];
+    const initialBlockPos = this.getInitialBlockPos(BOARD_WIDTH, this.block);
     this.x = initialBlockPos.x;
     this.y = initialBlockPos.y;
   }
 
-  rotate() {
-    this.block = this.block[0].map((_, i) => this.block.map((row) => row[i]).reverse());
+  rotate(board) {
+    const rotatedBlock = this.block[0].map((_, i) => this.block.map((row) => row[i]).reverse());
+    const rotatedWidth = rotatedBlock[0].length;
+    const rotatedHeight = rotatedBlock.length;
+    let newX = this.x;
+    let newY = this.y;
+
+    if (newX + rotatedWidth > BOARD_WIDTH) {
+      newX = BOARD_WIDTH - rotatedWidth;
+    }
+    if (newX < 0) {
+      newX = 0;
+    }
+    if (newY + rotatedHeight > BOARD_HEIGHT) {
+      newY = BOARD_HEIGHT - rotatedHeight;
+    }
+    if (newY < 0) {
+      newY = 0;
+    }
+
+    if (this.canMove(board, newX, newY, rotatedBlock)) {
+      this.block = rotatedBlock;
+      this.x = newX;
+      this.y = newY;
+    }
   }
 
   moveLeft(board) {
@@ -29,7 +50,40 @@ export default class Tetromino {
   moveDown(board) {
     if (this.canMove(board, this.x, this.y + 1, this.block)) {
       this.y += 1;
+    } else {
+      this.freeze(board);
     }
+  }
+
+  freeze(board) {
+    const { block, x, y } = this;
+    block.forEach((row, i) => {
+      row.forEach((col, j) => {
+        if (col !== 0) {
+          board[y + i][x + j] = col * 10;
+        }
+      });
+    });
+
+    const clearedRows = this.removeFullRows(board);
+    document.querySelector('my-tetris').score += SCORES[clearedRows] ?? 0;
+
+    this.block = Object.values(BLOCKS)[Math.floor(Math.random() * 7)];
+    const initialBlockPos = this.getInitialBlockPos(BOARD_WIDTH, this.block);
+    this.x = initialBlockPos.x;
+    this.y = initialBlockPos.y;
+  }
+
+  removeFullRows(board) {
+    let clearedRows = 0;
+    board.forEach((row, i) => {
+      if (row.every((cell) => cell >= 10)) {
+        clearedRows += 1;
+        board.splice(i, 1);
+        board.unshift(Array(BOARD_WIDTH).fill(0));
+      }
+    });
+    return clearedRows;
   }
 
   canMove(board, x, y, block) {
@@ -38,16 +92,11 @@ export default class Tetromino {
         if (block[row][col] !== 0) {
           const nextX = x + col;
           const nextY = y + row;
-          if (nextX < 0 || nextX >= BOARD_WIDTH || nextY < 0 || nextY >= BOARD_HEIGHT) {
+          if (nextX < 0 || nextX >= BOARD_WIDTH || nextY < 0 || nextY >= BOARD_HEIGHT || board[nextY][nextX] >= 10) {
             return false;
           }
         }
       }
-    }
-    const blockHeight = block.reduce((acc, row) => acc + row.some((cell) => cell !== 0), 0);
-    const bottomMostRowY = y + blockHeight - 1;
-    if (bottomMostRowY >= BOARD_HEIGHT) {
-      return false;
     }
 
     return true;
